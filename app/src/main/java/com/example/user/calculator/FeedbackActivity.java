f9f5d6d5a66bc8a79a92e36fd5e9ba17d1dda3e8
KirillBelitsky <kirill_belitsky@mail.ru>
2018-07-13 19:06:39 +0300

    Additional

--- app/src/main/java/com/example/user/calculator/FeedbackActivity.java
+++ app/src/main/java/com/example/user/calculator/FeedbackActivity.java
@@ -2,13 +2,19 @@ package com.example.user.calculator;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import javax.mail.MessagingException;
 
 import com.example.user.calculator.R;
+import com.example.user.calculator.utils.GMailSender;
+
+import static com.example.user.calculator.constants.Constants.SMTP_login;
+import static com.example.user.calculator.constants.Constants.SMTP_password;
 
 public class FeedbackActivity extends AppCompatActivity {
 
@@ -24,42 +30,66 @@ public class FeedbackActivity extends AppCompatActivity {
         subjectText=findViewById(R.id.subject_text);
         messageText=findViewById(R.id.message_text);
         sendButton=findViewById(R.id.send_button);
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email_text=emailText.getText().toString();
-                String subject=subjectText.getText().toString();
-                String message=messageText.getText().toString();
-
-                if(subject.equals("") && message.equals("")){
-                    Toast.makeText(getApplicationContext(),"Enter the message and subject!",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                else if(subject.equals("") || message.equals("")){
-                    if(subject.equals("")){
-                        Toast.makeText(getApplicationContext(),"Enter the subject!",Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    else{
-                        Toast.makeText(getApplicationContext(),"Enter the message!",Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                }
-
-                message="My email: "+email_text+"\n"+"Message:\n"+message;
-                subject="The application calculator:"+subject;
-                Intent intent=new Intent(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_EMAIL,new String[]{"kirill_belitsky@mail.ru"});
-                intent.putExtra(Intent.EXTRA_SUBJECT,subject);
-                intent.putExtra(Intent.EXTRA_TEXT,message);
-
-                intent.setType("message/rfc822");
-                startActivity(Intent.createChooser(intent,"Choose the email client"));
-            }
-        });
+        sendButton.setOnClickListener(v -> click());
     }
 
+    public void onBackPressed() {
+        finish();
+    }
 
+    public void click(){
 
+        String email_text=emailText.getText().toString();
+        String subject=subjectText.getText().toString();
+        String message=messageText.getText().toString();
+
+        if(subject.equals("") && message.equals("")){
+            Toast.makeText(getApplicationContext(),"Enter the message and subject!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if(subject.equals("") || message.equals("")){
+            if(subject.equals("")){
+                Toast.makeText(getApplicationContext(),"Enter the subject!",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else{
+                Toast.makeText(getApplicationContext(),"Enter the message!",Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        sendButton.setEnabled(false);
+        new sendEmail().execute();
+    }
+
+    private class sendEmail extends AsyncTask<Void, Void, Boolean> {
+
+        protected Boolean doInBackground(Void... voids) {
+            GMailSender sender = new GMailSender(SMTP_login,SMTP_password);
+
+            String message = "Subject: " +subjectText.getText().toString()
+                    + "\nEmail: " + emailText.getText().toString()
+                    + "\nMessage: " + messageText.getText().toString();
+
+            try {
+                sender.sendMail("Feedback", message);
+            } catch (MessagingException e) {
+                return false;
+            }
+            return true;
+        }
+
+        protected void onPostExecute(Boolean aBoolean) {
+            if (aBoolean) {
+                Toast.makeText(FeedbackActivity.this,"Thank you!",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                Toast.makeText(FeedbackActivity.this, "Error!",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+            sendButton.setEnabled(true);
+        }
+    }
 }
